c0d332280eb3aa855b551f5a80c023fcc9d8f3aa
Slawomir Kuzniar <slawomir.kuzniar@gmail.com>
2026-04-28 06:41:12 -0400

    Cleanup.

--- Apps/OUCH/OUCH50/LoginRejected.h
+++ Apps/OUCH/OUCH50/LoginRejected.h
@@ -16,28 +16,6 @@ struct LoginRejected
         SessionNotAvailable = 'S',
     };
 
-    friend std::string to_json(RejectCode o, bool verbose)
-    {
-        if (verbose) {
-            switch (o) {
-                    // clang-format off
-            	case RejectCode::NotAuthorized:       return to_json("'A'(NotAuthorized)"      , verbose); break;
-            	case RejectCode::SessionNotAvailable: return to_json("'S'(SessionNotAvailable)", verbose); break;
-            	default: return to_json(std::to_string(static_cast<std::underlying_type_t<RejectCode>>(o)) + "(Invalid LoginReject)", verbose); break;
-                    // clang-format on
-            };
-        } else {
-            switch (o) {
-                    // clang-format off
-            	case RejectCode::NotAuthorized:       return to_json('A', verbose); break;
-            	case RejectCode::SessionNotAvailable: return to_json('S', verbose); break;
-            	default: return to_json(static_cast<std::underlying_type_t<RejectCode>>(o), verbose); break;
-                    // clang-format on
-            };
-        }
-        return to_json("", verbose);
-    }
-
     PacketHeader header = { Type, sizeof(LoginRejected) };
     RejectCode   rejectReasonCode;
 };
